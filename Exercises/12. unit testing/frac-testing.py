5aeb6140b941d9956f9b368fbd1147b5878f2d1f
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-09-30 13:31:08 +0200

    testing generally ok

--- Exercises/12. unit testing/frac-testing.py
+++ Exercises/12. unit testing/frac-testing.py
@@ -39,19 +39,41 @@ class TestFrac(unittest.TestCase):
     
     def test_2_fracs_not_equal(self):
         f_1 = self.create_frac()
-        f_2 = Frac(2, 5)
-        self.assertNotEqual((f_1.nominator, f_1.denominator), (2,5))
-
-    """# test simplify(), which based on __eq__ is tested.
+        f_2 = Frac(1, 3)
+        self.assertNotEqual(f_1, f_2)
+    
+    # test simplify(), which based on __eq__ is tested.
     def test_2_fracs_equal(self):
         f_2 = self.create_frac()
-        f_1 = Frac(1, 2)
-        self.assertEqual((f_1.nominator, f_1.denominator), (f_2.nominator, f_2.denominator))"""
-
+        f_1 = Frac(2, 4)
+        self.assertEqual(f_1, f_2)
+        
     def test_simplify(self):
         f_1 = self.create_frac()
         f_2 = Frac(4,8)
         self.assertEqual(f_1, f_2.simplify())
+
+    def test_add(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(1,3)
+        self.assertEqual(f_1+f_2, Frac(5,6))
+
+    def test_sub(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(1,3)
+        self.assertEqual(f_1-f_2, Frac(1,6))
+
+    def test_truediv(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(1,4)
+        self.assertAlmostEqual((f_1/f_2), Frac(2,1))
+
+    def test_mul(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(1,4)
+        self.assertEqual((f_1*f_2), Frac(1,8))
+
+
 
 
 
--- Exercises/12. unit testing/test.py
+++ Exercises/12. unit testing/test.py
@@ -1,4 +1,10 @@
 import math
 
-highest_common_factor = math.gcd(6, 9)
+highest_common_factor = math.gcd(4, 2)
 print(highest_common_factor)
+
+nominator_simplified = int(4/ highest_common_factor)
+denominator_simplified = int(2 / highest_common_factor)
+
+print(nominator_simplified)
+print(denominator_simplified)
