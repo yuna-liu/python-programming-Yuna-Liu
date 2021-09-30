316bc2407c7393a6c4fb502afee33dd9a0e577ab
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-09-30 09:14:20 +0200

    new simplify

--- Exercises/12. unit testing/frac-testing.py
+++ Exercises/12. unit testing/frac-testing.py
@@ -2,15 +2,61 @@ from frac import Frac
 import unittest
 
 class TestFrac(unittest.TestCase):
-    """ def setUP(self) -> None:
+    def setUp(self) -> None:
         self.x, self.y = 1, 2
     def create_frac(self) -> "Frac":
         return Frac(self.x, self.y)
-    # testing starts here - all tests must start with the word test_
+
     def test_create_frac(self) -> None:
-        self.assertEqual(Frac(self.x, self.y),self.x/self.y)"""
-    def test_create_frac(self) -> None:
-        self.assertEqual(Frac(1, 2), 1/2)
+        f_1 = self.create_frac()
+        self.assertEqual((f_1.nominator, f_1.denominator),(self.x, self.y))
+
+    def test_negative_nominator(self):
+        with self.assertRaises(ValueError):
+            f = Frac(-1,6)
+
+    def test_negative_denominator(self):
+        with self.assertRaises(ValueError):
+            f = Frac(1,-6)
+
+    def test_zero_denominator(self):
+        with self.assertRaises(ValueError):
+            f = Frac(1,0)
+
+    def test_str_nominator(self):
+        with self.assertRaises(TypeError):
+            f = Frac("20",100)    
+    
+    def test_str_nominator(self):
+        with self.assertRaises(TypeError):
+            f = Frac(20,"100")
+
+# test __eq__ for two fractions that are equal.
+    def test_2_fracs_equal(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(1, 2)
+        self.assertEqual(f_1, f_2)
+    
+    def test_2_fracs_not_equal(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(2, 5)
+        self.assertNotEqual((f_1.nominator, f_1.denominator), (2,5))
+
+    """# test simplify(), which based on __eq__ is tested.
+    def test_2_fracs_equal(self):
+        f_2 = self.create_frac()
+        f_1 = Frac(1, 2)
+        self.assertEqual((f_1.nominator, f_1.denominator), (f_2.nominator, f_2.denominator))"""
+
+    def test_simplify(self):
+        f_1 = self.create_frac()
+        f_2 = Frac(4,8)
+        self.assertEqual(f_1, f_2.simplify())
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
