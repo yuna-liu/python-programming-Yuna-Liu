08f55cbfcafe12b7c48d73f3de7a32861650fce0
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-10-02 23:58:11 +0200

    create Rectangle(Shape) class

---
+++ Labs/Lab3.geometry-OOP/rectangle.py
@@ -0,0 +1,50 @@
+from geometry_shape import Shape
+
+class Rectangle(Shape):
+    def __init__(self, x: float, y: float, side1: float, side2: float, type="rectangle") -> None: 
+
+        #side1 is the length on the horizontal line of the rectangle.
+        #side2 is the length on the vertical line of the rectangle.
+
+        super().__init__(x,y,type)
+        self.side1 = side1
+        self.side2 = side2
+    
+    @property
+    def side1(self) -> float:
+        return self._side1
+    
+    @side1.setter
+    def side1(self, value: float) -> None:
+        self._side1 = Shape.validate_positive_number(value)
+
+    @property
+    def side2(self) -> float:
+        return self._side2
+    
+    @side2.setter
+    def side2(self, value: float) -> None:
+        self._side2 = Shape.validate_positive_number(value)
+    
+    def area(self) -> float:
+        return self.side1*self.side2
+    
+    def perimeter(self) -> float:
+        return 2*(self.side1+self.side2)
+
+    # return whether a point is in a shape
+    # x_point and y_point are checked valid through method Shape.hori_ver_dis()
+    def is_inside(self, x_point:float, y_point:float) -> bool:
+        if Shape.hori_ver_dis(self.x, x_point) <= 0.5*self.side1 and Shape.hori_ver_dis(self.y, y_point) <= 0.5*self.side2:
+            return True
+        else:
+            return False
+    
+    def __eq__(self, other) -> bool:
+        if self.type == other.type and self.area() == other.area() and self.side1 == other.side1 and self.side2 == other.side2:
+            return True
+        else:
+            return False
+
+    def __repr__(self) -> str:
+        return f"{self.type} with center point: ({self.x}, {self.y}) with (horizontal side, vertical side): ({self.side1}, {self.side2})."  
